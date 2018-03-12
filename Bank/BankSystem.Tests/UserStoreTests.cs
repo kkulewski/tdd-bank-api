21c4614d8f70e60433d640386531537e0361acdf
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-12 23:29:52 +0100

    Add get user on empty user store test + impl

---
+++ Bank/BankSystem.Tests/UserStoreTests.cs
@@ -0,0 +1,22 @@
+﻿using BankSystem.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankSystem.Tests
+{
+    [TestClass]
+    public class UserStoreTests
+    {
+        [TestMethod]
+        public void GetUserByLogin_ReturnsNull_WhenUserListIsEmpty()
+        {
+            // ARRANGE
+            IUserStore userStore = new InMemoryUserStore();
+
+            // ACT
+            var user = userStore.GetUserByLogin("someLogin");
+
+            // ASSERT
+            Assert.IsNull(user);
+        }
+    }
+}
---
+++ Bank/BankSystem/DAL/InMemoryUserStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BankSystem.Models;
+
+namespace BankSystem.DAL
+{
+    public class InMemoryUserStore : IUserStore
+    {
+        private static List<IUser> _users = new List<IUser>();
+
+        public IUser GetUserByLogin(string login)
+        {
+            return _users.FirstOrDefault(x => x.Login == login);
+        }
+    }
+}
