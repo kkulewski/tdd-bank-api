90f1c91e4da8e1a38485fd856fb964cfd9220375
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-13 21:49:59 +0100

    Add MS tests with MS Fakes + first user store test

---
+++ Bank/MSTests/UserStoreTests.cs
@@ -0,0 +1,25 @@
+﻿using BankSystem.DAL;
+using BankSystem.Models;
+using BankSystem.Models.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTests
+{
+    [TestClass]
+    public class UserStoreTests
+    {
+        [TestMethod]
+        public void Add_ReturnsTrue_WhenAddingUserToEmptyList()
+        {
+            // ARRANGE
+            var userStore = new InMemoryUserStore();
+            IUser user = new StubIUser();
+
+            // ACT
+            var result = userStore.Add(user);
+
+            // ASSERT
+            Assert.IsTrue(result);
+        }
+    }
+}
