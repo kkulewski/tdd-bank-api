3871c52d9208bbc3d23a769ac0c84ddb1d174d12
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-13 22:17:39 +0100

    Add auth test when user does not exist

---
+++ Bank/MSTests/AuthenticationServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BankSystem.DAL;
+using BankSystem.DAL.Fakes;
+using BankSystem.Models;
+using BankSystem.Models.Fakes;
+using BankSystem.Services.Authentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTests
+{
+    [TestClass]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod]
+        public void Authenticate_ReturnsFalse_WhenUserWithGivenLogin_IsNotFound()
+        {
+            // ARRANGE
+            IUserStore userStoreStub = new StubIUserStore
+            {
+                GetUserByLoginString = login => null
+            };
+
+            IUserFactory userFactoryStub = new StubIUserFactory();
+            IAuthenticationService authService = new AuthenticationService(userStoreStub, userFactoryStub);
+
+            // ACT
+            bool authResult = authService.Authenticate("someLogin", "somePassword");
+
+            // ASSERT
+            Assert.IsFalse(authResult);
+        }
+    }
+}
