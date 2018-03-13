6ea78305541b4107d6f9fdf5405fcc12b9e831b7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-13 01:43:11 +0100

    Test API methods

--- Bank/HostApp/Program.cs
+++ Bank/HostApp/Program.cs
@@ -12,6 +12,40 @@ namespace HostApp
         static void Main(string[] args)
         {
             var bank = GetApi();
+            Console.WriteLine("## CREATING ACCOUNTS");
+            Console.WriteLine(bank.SignUp("JohnDoe", "secret"));
+            Console.WriteLine(bank.SignUp("JohnDoe", "other"));
+            Console.WriteLine(bank.SignUp("KateDoe", "pass"));
+            Console.WriteLine(bank.SignUp("DaveSmith", "dave"));
+
+            Console.WriteLine("## SIGNING IN");
+            Console.WriteLine(bank.SignIn("wrong", "wrong"));
+            Console.WriteLine(bank.SignIn("JohnDoe", "wrong"));
+            Console.WriteLine(bank.SignIn("JohnDoe", "secret"));
+            
+            Console.WriteLine("## CHECKING BALANCE");
+            Console.WriteLine(bank.GetMyAccountBalance());
+
+            Console.WriteLine("## SENDING TRANSFER");
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", 1200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("wrong", 200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", -200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", 200.0M));
+
+            Console.WriteLine("## CHECKING BALANCE");
+            Console.WriteLine(bank.GetMyAccountBalance());
+
+            Console.WriteLine("## SIGNING OUT");
+            Console.WriteLine(bank.SignOut());
+            Console.WriteLine(bank.SignOut());
+            
+            Console.WriteLine("## SENDING TRANSFER");
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", 1200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("wrong", 200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", -200.0M));
+            Console.WriteLine(bank.SendMoneyTransfer("KateDoe", 200.0M));
+
+            Console.ReadKey();
         }
 
         private static BankApi GetApi()
