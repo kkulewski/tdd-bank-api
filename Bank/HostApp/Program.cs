a9c70482d67ae36fedfc9d1337e2a74a2bbc3c9e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-13 01:42:56 +0100

    Add demo app with API build method

---
+++ Bank/HostApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BankSystem;
+using BankSystem.DAL;
+using BankSystem.Models;
+using BankSystem.Services.Account;
+using BankSystem.Services.Authentication;
+
+namespace HostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bank = GetApi();
+        }
+
+        private static BankApi GetApi()
+        {
+            var userFactory = new UserFactory();
+            var userStore = new InMemoryUserStore();
+            var authService = new AuthenticationService(userStore, userFactory);
+            var accountService = new AccountService();
+            return new BankApi(authService, accountService, userStore);
+        }
+    }
+}
